9e38b6752cb634865546c7de8e2f969eb91ed731
Reboot93 <33769779+Reboot93@users.noreply.github.com>
2023-01-25 02:43:13 +0800

    自动分配定时器|Automatic allocation timer
    添加了自动分配定时器的功能
    在类中设置分配给Button类的定时器范围，调用定时器时将自动选择空闲的定时器。
    同时可以按下识别的按键数量取决与分配的定时器数量
    (machine translation)
    Added the function of automatic allocation timer
    Set the timer range assigned to the Button class in the class, and the idle timer will be automatically selected when calling the timer.
    The number of keys that can be recognized at the same time depends on the number of timers allocated

--- button.py
+++ button.py
@@ -29,18 +29,85 @@ SOFTWARE.
 
 from machine import Pin, Timer
 
-class Button:
 
-    def __init__(self, pin, single_click_time=130, long_press_time=210, pull=Pin.PULL_UP, trigger=Pin.IRQ_FALLING):
+class Button:
+    # 设置 定时器范围      eg. Timer(0),Timer(1),Timer(2),Timer(3) --> (0, 3)
+    # Set timer range
+    timer_number_range = (0, 3)
+
+    # ================================================================
+
+    timer_dict = {}
+    for i in range(timer_number_range[0], timer_number_range[1] + 1):
+        timer_dict[i] = 0
+    timer_count = 0
+    timer_quantity = len(timer_dict)
+
+    timer_irp_count = 0
+
+    # pin 为 按键 GPIO 号， single_click_time 与 long_press_time 为 单击检测时长 与 长按检测时长（具体时长取决于定时器设置的触发时间）
+    # Pin is the key GPIO number,
+    # single_ click_ Time and long_ press_ Time is the click detection duration and press detection duration
+    # (the specific duration depends on the trigger time set by the timer)
+    def __init__(self, pin, single_click_time=130, long_press_time=210, pull=Pin.PULL_DOWN, trigger=Pin.IRQ_RISING):
         self.pin = pin
         self.callback = None
         self.isEnable = False
-        self._single_click_time = single_click_time
+        self._short_press_time = single_click_time
         self._long_press_time = long_press_time
         self._timer_count = 0
         self.button = Pin(pin, Pin.IN, pull)
         self.trigger = trigger
-        self._long_press_timer = Timer(0)
+        self._long_press_timer = None
+        self._timer_number = None
+        if pull == Pin.PULL_DOWN:
+            self._timer_bt_value = 1
+        else:
+            self._timer_bt_value = 0
+
+    def __get_available_timer(self):
+        date_value = list(Button.timer_dict.values())
+        try:
+            i = Button.timer_irp_count
+            while i > 0:
+                index = date_value.index(0)
+                date_value = date_value[index:]
+                i -= 1
+            date_key = list(Button.timer_dict.keys())
+            return date_key[index]
+        except ValueError:
+            return None
+
+    def __setEnable(self, flag: bool):
+        if flag:
+            self.button.irq(handler=self._irq_callback, trigger=self.trigger)
+            self.isEnable = True
+            self._long_press_timer = None
+            number = self._timer_number
+            self._timer_number = None
+            Button.timer_dict[number] = 0
+            Button.timer_count -= 1
+        else:
+            if Button.timer_count > Button.timer_quantity - 1:
+                return 1
+            Button.timer_irp_count += 1
+            timer_number = self.__get_available_timer()
+            if self._timer_number is not None:
+                Button.timer_irp_count -= 1
+                return 3
+            if timer_number is not None:
+                Button.timer_dict[timer_number] = 1
+                Button.timer_irp_count -= 1
+                self.button.irq(handler=None)
+                self.isEnable = False
+                self._long_press_timer = Timer(timer_number)
+                self._timer_number = timer_number
+                Button.timer_count += 1
+                return 0
+            else:
+                Button.timer_irp_count -= 1
+                return 2
+
 
     def setEnable(self, flag: bool):
         if flag:
@@ -54,25 +121,34 @@ class Button:
         self.callback = fun
 
     def _irq_callback(self, pin):
-        self.setEnable(False)
-        self.click_flag = False
-        self.long_press_flag = False
-        self._long_press_timer.init(period=2, callback=self._timer_irp_callback)
+        self.button.irq(handler=None)
+        i = self.__setEnable(False)
+        if i != 0:
+            # print("No more timer, please wait for release")
+            self.setEnable(True)
+            return
+        else:
+            self.click_flag = False
+            self.long_press_flag = False
+            self._long_press_timer.init(period=2, callback=self._timer_irp_callback)
 
     def _timer_irp_callback(self, msg):
-        if self.button.value() != 0 or self._timer_count > self._long_press_time + 10:
-            print(self._timer_count)
-            if 5 < self._timer_count < self._single_click_time:
+        if self.button.value() != self._timer_bt_value or self._timer_count > self._long_press_time:
+            # print(self._timer_count)
+            if 5 < self._timer_count < self._short_press_time:
                 self._long_press_timer.deinit()
-                print('Button %s clicked' % str(self.pin))
+                print('Button %s short press' % str(self.pin))
                 self.callback(self.pin, 0)
             elif self._timer_count > self._long_press_time:
                 self._long_press_timer.deinit()
                 print('Button %s long pressed' % str(self.pin))
                 self.callback(self.pin, 1)
             else:
-                self._long_press_timer.deinit()
+                try:
+                    self._long_press_timer.deinit()
+                except AttributeError:
+                    pass
             self._timer_count = 0
-            self.setEnable(True)
+            self.__setEnable(True)
         else:
             self._timer_count += 1
