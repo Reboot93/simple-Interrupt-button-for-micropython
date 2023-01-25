f95ee7765e6d0322c3fe1595842d2ae02afef61d
Reboot93 <33769779+Reboot93@users.noreply.github.com>
2023-01-26 00:09:06 +0800

    添加“按住”检测 | Add "hold " detection
    1.添加“按住”检测 | Add "hold " detection
          msg = 3 : hold
          msg = 4 : release
    2.优化长按所需时间判断方式 | Optimize the judgment method of long press
    3.可以再初始化时通过关键字参数设置定时器间隔 | The timer interval can be set by keyword parameters during reinitialization

--- button.py
+++ button.py
@@ -1,23 +1,17 @@
 '''
 simple-Interrupt-button-for-micropython
-
 https://github.com/Reboot93/simple-Interrupt-button-for-micropython
-
 ====================================================
 MIT License
-
 Copyright (c) 2022 Reboot93
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,6 +22,12 @@ SOFTWARE.
 '''
 
 from machine import Pin, Timer
+
+
+# callback msg : 1: single click
+#                2: long click
+#                3: hold
+#                4: release
 
 
 class Button:
@@ -45,21 +45,24 @@ class Button:
 
     timer_irp_count = 0
 
-    # pin 为 按键 GPIO 号， single_click_time 与 long_press_time 为 单击检测时长 与 长按检测时长（具体时长取决于定时器设置的触发时间）
+    # pin 为 按键 GPIO 号， single_click_time 与 press_hold_time 为 单击最大时长 与 按住开始时长（具体时长取决于定时器设置的触发时间），二者之间为长按
     # Pin is the key GPIO number,
-    # single_ click_ Time and long_ press_ Time is the click detection duration and press detection duration
+    # single_click_time and press_hold_time is the click detection duration and press detection duration
     # (the specific duration depends on the trigger time set by the timer)
-    def __init__(self, pin, single_click_time=130, long_press_time=210, pull=Pin.PULL_DOWN, trigger=Pin.IRQ_RISING):
+    def __init__(self, pin, single_click_time=100, press_hold_time=350, timer_interval=2, pull=Pin.PULL_DOWN,
+                 trigger=Pin.IRQ_RISING):
         self.pin = pin
         self.callback = None
         self.isEnable = False
         self._short_press_time = single_click_time
-        self._long_press_time = long_press_time
+        self._hold_time = press_hold_time
         self._timer_count = 0
+        self._timer_interval = timer_interval
         self.button = Pin(pin, Pin.IN, pull)
         self.trigger = trigger
         self._long_press_timer = None
         self._timer_number = None
+        self._flag_hold = False
         if pull == Pin.PULL_DOWN:
             self._timer_bt_value = 1
         else:
@@ -83,9 +86,8 @@ class Button:
             self.button.irq(handler=self._irq_callback, trigger=self.trigger)
             self.isEnable = True
             self._long_press_timer = None
-            number = self._timer_number
+            Button.timer_dict[self._timer_number] = 0
             self._timer_number = None
-            Button.timer_dict[number] = 0
             Button.timer_count -= 1
         else:
             if Button.timer_count > Button.timer_quantity - 1:
@@ -108,7 +110,6 @@ class Button:
                 Button.timer_irp_count -= 1
                 return 2
 
-
     def setEnable(self, flag: bool):
         if flag:
             self.button.irq(handler=self._irq_callback, trigger=self.trigger)
@@ -122,27 +123,46 @@ class Button:
 
     def _irq_callback(self, pin):
         self.button.irq(handler=None)
-        i = self.__setEnable(False)
-        if i != 0:
+        if self.__setEnable(False) != 0:
             # print("No more timer, please wait for release")
             self.setEnable(True)
             return
         else:
             self.click_flag = False
             self.long_press_flag = False
-            self._long_press_timer.init(period=2, callback=self._timer_irp_callback)
+            self._long_press_timer.init(period=self._timer_interval, callback=self._timer_irp_callback)
 
     def _timer_irp_callback(self, msg):
-        if self.button.value() != self._timer_bt_value or self._timer_count > self._long_press_time:
+        if self.button.value() != self._timer_bt_value or self._timer_count > self._hold_time:
             # print(self._timer_count)
-            if 5 < self._timer_count < self._short_press_time:
+            if 5 < self._timer_count <= self._short_press_time:
+                # Single Click 
                 self._long_press_timer.deinit()
                 print('Button %s short press' % str(self.pin))
                 self.callback(self.pin, 0)
-            elif self._timer_count > self._long_press_time:
+            elif self._short_press_time < self._timer_count <= self._hold_time:
+                # Long Click 
                 self._long_press_timer.deinit()
                 print('Button %s long pressed' % str(self.pin))
                 self.callback(self.pin, 1)
+            elif self._timer_count > self._hold_time:
+                if self._flag_hold:
+                    # 非初次 进入hold判断
+                    if self.button.value() != self._timer_bt_value:
+                        self._long_press_timer.deinit()
+                        print('Button %s release' % str(self.pin))
+                        self._flag_hold = False
+                        self.callback(self.pin, 4)
+                    else:
+                        self._timer_count += 1
+                        return
+                else:
+                    # 初次 进入hold判断
+                    self._flag_hold = True
+                    print('Button %s Hold' % str(self.pin))
+                    self.callback(self.pin, 3)
+                    self._timer_count += 1
+                    return
             else:
                 try:
                     self._long_press_timer.deinit()
